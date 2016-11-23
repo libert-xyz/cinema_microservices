7b2955294dcbb7ac43dc632134a4e492de2435be
Libert R Schmidt <rschmidt@nuvops.com>
2016-11-22 21:49:22 -0500

    Movies services POST / PUT /DELETE

--- services/movies/movies.py
+++ services/movies/movies.py
@@ -1,8 +1,7 @@
 from settings import app,db
 from models import Movies
 import json
-from flask import jsonify, request,make_response,abort
-
+from flask import jsonify, request,make_response,abort, abort
 
 @app.route('/movies',methods=['GET'])
 def movies():
@@ -11,14 +10,55 @@ def movies():
         #return jsonify(Movies = [i.serialize for i in movies])
         return jsonify(Movies = [i.serialize for i in movies])
 
-@app.route('/movies/<int:id>', methods=['GET'])
+@app.route('/movies/<int:id>', methods=['GET','PUT','DELETE'])
 def get_movie(id):
 
     try:
          movie = Movies.query.filter_by(id=id).one()
-         return jsonify(Movie = movie.serialize)
+         if request.method == 'GET':
+             return jsonify(Movie = movie.serialize)
+
+         elif request.method == 'PUT':
+            title = request.args.get('title')
+            rating = request.args.get('rating')
+
+            if title:
+                movie.title = title
+
+            if rating:
+                movie.rating = rating
+
+            db.session.add(movie)
+            db.session.commit()
+
+            return jsonify(Movie = movie.serialize)
+
+         elif request.method == 'DELETE':
+             db.session.delete(movie)
+             db.session.commit()
+             return 'Movie Deleted'
     except:
         abort(404)
+
+@app.route('/movies',methods=['POST','PUT','DELETE'])
+def new_movies():
+
+    title = request.args.get('title','')
+    rating = request.args.get('rating','')
+    if title == '' or rating == '':
+        abort(400)
+    if Movies.query.filter_by(title=title).first() is not None:
+
+        return jsonify({"message":"movie alaredy exists"}), 200
+
+    movie = Movies(title=title)
+    movie.rating = rating
+    db.session.add(movie)
+    db.session.commit()
+
+    return jsonify({'Title': movie.title,'Raiting': movie.rating, 'id': movie.id})
+
+
 
 @app.errorhandler(404)
 def not_found(error):
--- services/movies/test.py
+++ services/movies/test.py
@@ -14,11 +14,11 @@ class TestMoviesService(unittest.TestCase):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
         db.create_all()
         self.app = app.test_client()
-        self.url = 'http://localhost://127.0.0.1/movies'
+        self.url = 'http://localhost:5000/movies'
 
 
     def tearDown(self):
-        #db.session.remove()
+        db.session.remove()
         db.drop_all()
 
     def test_MoviesAll(self):
@@ -36,11 +36,60 @@ class TestMoviesService(unittest.TestCase):
         db.session.add(m3)
         db.session.commit()
 
-        #response = tester.get('http://localhost://127.0.0.1/movies')
         response = tester.get('/movies')
 
+        #response = requests.get("{}".format(self.url))
+        #response = response.json()
+
+
         print(response.data)
-        self.assertIn(b'FightClub',response.data)
+        #self.assertIn(b'FightClub',response.data)
+        self.assertEqual(b'{\n  "Movies": [\n    {\n      "Raiting": 2, \n      "Title": "Dragon Ball", \n      "id": 1\n    }, \n    {\n      "Raiting": 10, \n      "Title": "FightClub", \n      "id": 2\n    }\n  ]\n}\n',
+        response.data)
+        #self.asserEqual(b"{'id': 4, 'Raiting': 10, 'Title': 'FightClub'}",response['Movies'][3])
+
+    def test_movies_id(self):
+        tester = app.test_client(self)
+
+        m2 = Movies(title='Dragon Ball')
+        m2.rating=2
+        db.session.add(m2)
+        db.session.commit()
+
+        response = tester.get('/movies/1')
+        print(response.data)
+        self.assertIn(b'Dragon Ball',response.data)
+
+    def test_movies_post(self):
+        tester = app.test_client(self)
+
+        response = tester.post('/movies?title=DragonBall&rating=4')
+        print (response.data)
+        self.assertIn(b'DragonBall',response.data)
+
+
+    def test_movies_put(self):
+        tester = app.test_client(self)
+        m = Movies(title='Dragon Ball')
+        m.rating = 5
+        db.session.add(m)
+        db.session.commit()
+
+        response = tester.put('/movies/1?title=Pokemon')
+        print (response.data)
+        self.assertIn(b'Pokemon',response.data)
+
+
+    def test_movies_delete(self):
+        tester = app.test_client(self)
+        m = Movies(title='Dragon Ball')
+        m.rating = 2
+        db.session.add(m)
+        db.session.commit()
+
+        response = tester.delete('/movies/1')
+        print(response.data)
+        self.assertIn(b'Movie Deleted',response.data)
 
 
 
